6ea2126b2f9c469e72624241aa4b932c39f346f2
gizayakbuyuk <gizaykata@gmail.com>
2016-03-17 19:04:25 +0200

    Added files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Sayıları_Yazıya_Çevir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCevir = new System.Windows.Forms.Button();
+            this.textBoxSayi = new System.Windows.Forms.TextBox();
+            this.textBoxMetin = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonCevir
+            // 
+            this.buttonCevir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.buttonCevir.Location = new System.Drawing.Point(416, 20);
+            this.buttonCevir.Name = "buttonCevir";
+            this.buttonCevir.Size = new System.Drawing.Size(83, 34);
+            this.buttonCevir.TabIndex = 0;
+            this.buttonCevir.Text = "Çevir";
+            this.buttonCevir.UseVisualStyleBackColor = true;
+            this.buttonCevir.Click += new System.EventHandler(this.buttonCevir_Click);
+            // 
+            // textBoxSayi
+            // 
+            this.textBoxSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxSayi.Location = new System.Drawing.Point(28, 23);
+            this.textBoxSayi.MaxLength = 4;
+            this.textBoxSayi.Name = "textBoxSayi";
+            this.textBoxSayi.Size = new System.Drawing.Size(370, 31);
+            this.textBoxSayi.TabIndex = 1;
+            this.textBoxSayi.TextChanged += new System.EventHandler(this.textBoxSayi_TextChanged);
+            this.textBoxSayi.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxSayi_KeyPress);
+            // 
+            // textBoxMetin
+            // 
+            this.textBoxMetin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxMetin.Location = new System.Drawing.Point(28, 67);
+            this.textBoxMetin.Multiline = true;
+            this.textBoxMetin.Name = "textBoxMetin";
+            this.textBoxMetin.Size = new System.Drawing.Size(471, 277);
+            this.textBoxMetin.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 449);
+            this.Controls.Add(this.textBoxMetin);
+            this.Controls.Add(this.textBoxSayi);
+            this.Controls.Add(this.buttonCevir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCevir;
+        private System.Windows.Forms.TextBox textBoxSayi;
+        private System.Windows.Forms.TextBox textBoxMetin;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sayıları_Yazıya_Çevir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxSayi_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void textBoxSayi_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //e.Handled = true; // basılan tuş iptal edilir
+            //e.Handled = false; // basılan tuş kabul edilir
+            int tus = (int)e.KeyChar;
+            if ((tus > 47 && tus < 58) || (tus == 8) )
+                e.Handled = false;
+            else
+                e.Handled = true;
+            
+        }
+
+        private void textBoxSayi_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCevir_Click(object sender, EventArgs e)
+        {
+            string[] birler = { "sıfır", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz" };
+            string[] onlar = { "","on", "yirmi", "otuz", "kırk", "elli", "atmış", "yetmiş", "seksen", "doksan" };
+
+            string sayi = textBoxSayi.Text.PadLeft(4,'0');
+
+            int[] bsmk = new int[sayi.Length];
+            for (int i = 0; i < sayi.Length; i++)
+                bsmk[i] = int.Parse(sayi[3-i].ToString());
+
+            textBoxMetin.Text = ((bsmk[3] == 0) ? "" : ((bsmk[3] == 1) ? "bin" : birler[bsmk[3]] + "bin")) + ((bsmk[2] == 0) ? "" : ((bsmk[2] == 1) ? "yüz" : birler[bsmk[2]] + "yüz")) + onlar[bsmk[1]] + ((bsmk[0] == 0) ? "" : birler[bsmk[0]]);
+
+
+        }
+    }
+}
